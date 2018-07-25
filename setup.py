e960632ff09f6df1da7d920e72076bd084c740d9
Rodrigo Souza <pinhopro@users.noreply.github.com>
2018-07-25 14:52:40 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+version = "0.1"
+
+REQUIREMENTS = [
+    "requests>=2.3.0",
+    "fire"
+]
+
+setup(
+  name="blinktrade-cli",
+  version=version,
+  author="Rodrigo Souza",
+  packages = [
+    "veloxbot"
+    ],
+  entry_points = { 'console_scripts':
+                     [
+                       'veloxbot blinktrade-cli.blinktrade-cli:main'
+                     ]
+  },
+  install_requires=REQUIREMENTS,
+  author_email='contact@blinktrade.com',
+  url='https://github.com/blinktrade/blinktrade-cli',
+  description='BlinkTrade client'
+)
